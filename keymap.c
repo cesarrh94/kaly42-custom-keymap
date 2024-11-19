7fcdadf3cff45f3b65dbb280c09e7eef66be7454
Cesar Rodriguez Hernandez <45974285+cesarrh94@users.noreply.github.com>
2024-11-18 19:53:51 -0600

    Rearrangement on the 3rd Layer (raise) for Keys F11 and F12

--- keymap.c
+++ keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      /* _RAISE Layer Keymap - Function, Modifiers & Media keys
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │F12│F1 │F2 │F3 │F4 │F5 │       │F6 │F7 │F8 │F9 │F10│F11│
+      * │Esc│F1 │F2 │F3 │F4 │F5 │       │F6 │F7 │F8 │F9 │F10│Bsp│
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
       * │Tab│   │Ins│Hom│Pup│   │       │   │Mut│Vld│Vlu│   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
 
     [_RAISE] = LAYOUT_split_3x6_3(
-         KC_F12,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-        _______, XXXXXXX, KC_INS, KC_HOME, KC_PGUP, XXXXXXX,                             XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
+        _______,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
+        _______, XXXXXXX, KC_INS, KC_HOME, KC_PGUP,  KC_F11,                              KC_F12, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, KC_DEL,  KC_END, KC_PGDN, XXXXXXX,                             XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, _______,
                                             XXXXXXX, _______, XXXXXXX,          _______, _______, _______
     ),
