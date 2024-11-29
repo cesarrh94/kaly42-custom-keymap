c9b7361632677ab61550c39f35271c788706e2a4
cesar rodriguez <94cesarrh@gmail.com>
2024-11-28 23:18:38 -0600

    New Keymap with Homerow Mods Implementation

--- keymap.c
+++ keymap.c
@@ -11,69 +11,69 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      
-     /* _BASE Layer Keymap - Alphas & Modifiers Keys
+     /* _BASE Layer Keymap - Alphas & Homerow Mods Implementation.
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Esc│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
+      * │ = │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ - │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Tab│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
+      * │Esc│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Alt│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Gui│
+      * │Tab│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Bsp│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │Mo1├───┐           ┌───┤Mo2│
-      *               └───┤Ctl├───┐   ┌───┤Sft├───┘
+      *               │ [ ├───┐           ┌───┤ ] │
+      *               └───┤Mo1├───┐   ┌───┤Mo2├───┘
       *                   └───┤Spc│   │Ent├───┘
       *                       └───┘   └───┘
       */
 
     [_BASE] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
-                                            MO(_LOWER), KC_LCTL, KC_SPC,    KC_ENT, KC_RSFT, MO(_RAISE)
+        KC_EQL,          KC_Q,         KC_W,          KC_E,          KC_R,    KC_T,                               KC_Y,          KC_U,          KC_I,          KC_O,             KC_P,    KC_MINS,
+        KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),    KC_G,                               KC_H,  RSFT_T(KC_J),  RCTL_T(KC_K),  RALT_T(KC_L),  RGUI_T(KC_SCLN),    KC_QUOT,
+        KC_TAB,          KC_Z,         KC_X,          KC_C,          KC_V,    KC_B,                               KC_N,          KC_M,       KC_COMM,        KC_DOT,          KC_SLSH,    KC_BSPC,
+                                                                 KC_LBRC , MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_RBRC 
     ),
 
-     /* _LOWER Layer Keymap - Number row, Modifiers & Arrow keys.
+     /* _LOWER Layer Keymap - Number row, Multimedia & Arrow keys.
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │Bsp│
+      * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │ \ │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Tab│   │ ` │ - │ = │   │       │   │   │ ↑ │   │   │   │
+      * │Esc│   │Mut│Vld│Vlu│   │       │   │   │ ↑ │   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Alt│   │ [ │ ] │ \ │   │       │   │ ← │ ↓ │ → │   │Gui│
+      * │Tab│   │Ply│Prv│Nxt│   │       │   │ ← │ ↓ │ → │   │Bsp│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │Mo1├───┐           ┌───┤   │
-      *               └───┤Ctl├───┐   ┌───┤Sft├───┘
+      *               │   ├───┐           ┌───┤   │
+      *               └───┤Mo1├───┐   ┌───┤   ├───┘
       *                   └───┤Spc│   │Bsp├───┘
       *                       └───┘   └───┘
       */
 
     [_LOWER] = LAYOUT_split_3x6_3(
-        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
-        _______, XXXXXXX,  KC_GRV, KC_MINS,  KC_EQL, XXXXXXX,                            XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX,                            XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______,
-                                            _______, _______, _______,          KC_BSPC, _______, XXXXXXX
+         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
+        _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                            XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX,                            XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______,
+                                            XXXXXXX, _______, _______,          KC_BSPC, XXXXXXX, XXXXXXX
     ),
 
-     /* _RAISE Layer Keymap - Function, Modifiers & Media keys
+     /* _RAISE Layer Keymap - Function, Arrow & Navigation keys.
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Esc│F1 │F2 │F3 │F4 │F5 │       │F6 │F7 │F8 │F9 │F10│Bsp│
+      * │F12│F1 │F2 │F3 │F4 │F5 │       │F6 │F7 │F8 │F9 │F10│F11│
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Tab│   │Ins│Hom│Pup│   │       │   │Mut│Vld│Vlu│   │   │
+      * │Esc│   │   │ ↑ │   │   │       │   │Ins│Hom│Pup│   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Alt│   │Del│End│Pdn│   │       │   │Ply│Prv│Nxt│   │Gui│
+      * │Tab│   │ ← │ ↓ │ → │   │       │   │Del│End│Pdn│   │Bsp│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │   ├───┐           ┌───┤Mo2│
-      *               └───┤Ctl├───┐   ┌───┤Sft├───┘
+      *               │   ├───┐           ┌───┤   │
+      *               └───┤   ├───┐   ┌───┤Mo2├───┘
       *                   └───┤   │   │Ent├───┘
       *                       └───┘   └───┘
       */
 
     [_RAISE] = LAYOUT_split_3x6_3(
-        _______,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
-        _______, XXXXXXX, KC_INS, KC_HOME, KC_PGUP,  KC_F11,                              KC_F12, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC_DEL,  KC_END, KC_PGDN, XXXXXXX,                             XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, _______,
-                                            XXXXXXX, _______, XXXXXXX,          _______, _______, _______
+         KC_F12,   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,                                KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+        _______, XXXXXXX, XXXXXXX,    KC_UP, XXXXXXX, XXXXXXX,                              XXXXXXX,  KC_INS, KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_LEFT,  KC_DOWN, KC_RGHT, XXXXXXX,                              XXXXXXX,  KC_DEL,  KC_END, KC_PGDN, XXXXXXX, _______,
+                                             XXXXXXX, XXXXXXX, XXXXXXX,          _______, _______, XXXXXXX
     ),
 };
