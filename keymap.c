0b2ba6633e8c252e39fe231f38eea2bb240fb6e1
cesar rodriguez <94cesarrh@gmail.com>
2024-12-02 11:16:01 -0600

    Final Key-mapping

--- keymap.c
+++ keymap.c
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
          KC_ESC,          KC_Q,         KC_W,          KC_E,          KC_R,    KC_T,                               KC_Y,          KC_U,          KC_I,          KC_O,             KC_P,    KC_BSPC,
          KC_TAB,  LGUI_T(KC_A), LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),    KC_G,                               KC_H,  RSFT_T(KC_J),  RCTL_T(KC_K),  RALT_T(KC_L),  RGUI_T(KC_SCLN),    KC_QUOT,
-        KC_LBRC,          KC_Z,         KC_X,          KC_C,          KC_V,    KC_B,                               KC_N,          KC_M,       KC_COMM,        KC_DOT,          KC_SLSH,    KC_RBRC,
-                                                                  KC_MINS , MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_EQL 
+        KC_MINS,          KC_Z,         KC_X,          KC_C,          KC_V,    KC_B,                               KC_N,          KC_M,       KC_COMM,        KC_DOT,          KC_SLSH,     KC_EQL,
+                                                                   KC_LBRC, MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_RBRC
     ),
 
      /* _LOWER Layer Keymap - Number row, Multimedia & Arrow keys.
